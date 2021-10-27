bf920d3b4b4ca643bc76d0c941352b2d4d4ea6f0
Viktoriia Starynchuk <Viktoriia.Starynchuk.IR.2021@lpnu.ua>
2021-10-27 15:41:12 +0300

    add lab3 code

---
+++ lab3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void printIntersection(int arrayA[], int arrayB[])
+{
+ int i = 0, j = 0;
+    while (i < 5 && j < 5) {
+        if (arrayA[i] < arrayB[j])
+            i++;
+        else if (arrayB[j] < arrayA[i])
+            j++;
+        else /* if arrayA[i] == arrayB[j] */
+        {
+            printf(" %d ", arrayB[j++]);
+            i++;
+        }
+    }
+}
+
+int main(){
+  int i;
+    int arrayA[5], arrayB[5];
+    printf("\nEnter array A elements : ");
+for (i = 0; i < 5; i++)
+ {
+    scanf_s("%d", &arrayA[i]);
+ }
+printf("\nEnter array B elements : ");
+for (i = 0; i < 5; i++)
+ {
+    scanf_s("%d", &arrayB[i]);
+ }
+printf("\nIntersection is : ");
+printIntersection(arrayA, arrayB, 5, 5);
+}
+
+
+
+
+
+
+
