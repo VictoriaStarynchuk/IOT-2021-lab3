130f2ffa47c71d157b2314c3eacbe7979ab23dd0
VictoriaStarynchuk <91877111+VictoriaStarynchuk@users.noreply.github.com>
2021-10-28 16:41:26 +0300

    update 1  lab.c

--- lab3.c
+++ lab3.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void printIntersection(int arrayA[], int arrayB[])
+void printIntersection(int arrayA[5], int arrayB[5])        //find intersection
 {
  int i = 0, j = 0;
-    while (i < 5 && j < 5) {
+    while (i < 5 && j < 5) {                            
         if (arrayA[i] < arrayB[j])
             i++;
         else if (arrayB[j] < arrayA[i])
             j++;
-        else /* if arrayA[i] == arrayB[j] */
+        else                                  // if arrayA[i] == arrayB[j]
         {
             printf(" %d ", arrayB[j++]);
             i++;
@@ -17,8 +17,7 @@ void printIntersection(int arrayA[], int arrayB[])
 }
 
 int main(){
-  int i;
-    int arrayA[5], arrayB[5];
+  int i, j, arrayA[5], arrayB[5];
     printf("\nEnter array A elements : ");
 for (i = 0; i < 5; i++)
  {
@@ -29,7 +28,7 @@ for (i = 0; i < 5; i++)
  {
     scanf_s("%d", &arrayB[i]);
  }
-printf("\nIntersection is : ");
+printf("\nIntersection of two arrays is : ");
 printIntersection(arrayA, arrayB, 5, 5);
 }
 
